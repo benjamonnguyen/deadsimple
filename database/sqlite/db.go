1892ee6da96f98b4deaaee4da8ba9ba7119f2eaf
benjamonnguyen <ben85629@gmail.com>
2025-10-24 20:47:41 -0700

    initial commit

---
+++ database/sqlite/db.go
@@ -0,0 +1,71 @@
+// Package sqlite
+package sqlite
+
+import (
+	"database/sql"
+	"embed"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	migratesql "github.com/golang-migrate/migrate/v4/database/sqlite"
+
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+	_ "modernc.org/sqlite"
+)
+
+type database struct {
+	// TODO bring your own logger
+	conn *sql.DB
+}
+
+func (db *database) Conn() *sql.DB {
+	return db.conn
+}
+
+func Open(url string) (*database, error) {
+	_, err := os.Stat(url)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("creating db file at", url)
+	file, err := os.OpenFile(url, os.O_CREATE, 0744)
+	if err != nil {
+		return nil, err
+	}
+	file.Close()
+
+	conn, err := sql.Open("sqlite", url)
+	if err != nil {
+		return nil, err
+	}
+	return &database{
+		conn: conn,
+	}, nil
+}
+
+// RunMigrations expects an embedded folder of sql files
+func (db *database) RunMigrations(migrations embed.FS) error {
+	d, err := migratesql.WithInstance(db.conn, &migratesql.Config{})
+	if err != nil {
+		return err
+	}
+	fs, err := iofs.New(migrations, "migrations")
+	if err != nil {
+		return err
+	}
+	m, err := migrate.NewWithInstance(
+		"iofs", fs,
+		"sqlite", d)
+	if err != nil {
+		return err
+	}
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil
+}
+
+func (db *database) Close() error {
+	return db.conn.Close()
+}
